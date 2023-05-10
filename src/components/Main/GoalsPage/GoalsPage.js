7f431a605ff142816db3738b7ccd734caefff60e
Krima222 <manackaakristina@gmail.com>
2023-05-10 17:11:53 +0300

    add all components for navigation

---
+++ src/components/Main/GoalsPage/GoalsPage.js
@@ -0,0 +1,7 @@
+
+
+export function GoalsPage() {
+    return (
+        <div>GoalsPage</div>
+    )
+}
---
+++ src/components/Main/SettingsPage/SettingsPage.js
@@ -0,0 +1,7 @@
+
+
+export function SettingsPage() {
+    return (
+        <div>Settings</div>
+    )
+}
