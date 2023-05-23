88d6969fea016ace4979c8b0602a7b73102b1f71
Krima222 <manackaakristina@gmail.com>
2023-05-23 21:08:27 +0300

    add dynamic goldcard rendering in feed

--- src/components/Main/NewsFeedPage/NewsFeedPage.js
+++ src/components/Main/NewsFeedPage/NewsFeedPage.js
@@ -1,21 +1,25 @@
 import { GoalCard } from '../GoalCard/GoalCard'
 import { feedData } from '../../../mockData/Feed'
 
-// const feedCards = feedData.map((item) => { 
-//     <GoalCard
-//         id={item.id}
-//         name={item.name}
-//         title={item.title}
-//         description={item.description}
-//         imageUrl={item.imageUrl}
-//         time={item.time}
-//         progress={item.progress}
-//     />
-// })
+import classes from './NewsFeedPage.module.scss'
+
+const feedCards = feedData.map((item) => (
+    <GoalCard
+        id={item.id}
+        name={item.name}
+        title={item.title}
+        description={item.description}
+        imageUrl={item.imageUrl}
+        time={item.time}
+        progress={item.progress}
+    />
+))
+
 export function NewsFeedPage() {
     
     return (
-        // <>{feedCards}</>
-        <div>GoalCard</div>
+        <div className={classes.feed}>
+            {feedCards}
+        </div>
     )
 }
