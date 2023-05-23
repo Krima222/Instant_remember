a4842e02b11db8056d8a72b50aba708065a83163
gurevnind <gurevnind@gmail.com>
2023-05-23 20:44:04 +0300

    отрисованна карточка поста

--- src/components/Main/NewsFeedPage/NewsFeedPage.js
+++ src/components/Main/NewsFeedPage/NewsFeedPage.js
@@ -1,6 +1,21 @@
+import { GoalCard } from '../GoalCard/GoalCard'
+import { feedData } from '../../../mockData/Feed'
 
+// const feedCards = feedData.map((item) => { 
+//     <GoalCard
+//         id={item.id}
+//         name={item.name}
+//         title={item.title}
+//         description={item.description}
+//         imageUrl={item.imageUrl}
+//         time={item.time}
+//         progress={item.progress}
+//     />
+// })
 export function NewsFeedPage() {
+    
     return (
-        <div>NewsFeedPage</div>
+        // <>{feedCards}</>
+        <div>GoalCard</div>
     )
 }
