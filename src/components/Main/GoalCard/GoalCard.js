fd0b0fd4f18db695e0684a29fbb15065abd984ec
lZoLoTol <ShahovArtem27@yandex.ru>
2023-05-30 03:06:54 +0300

    ...

--- src/components/Main/GoalCard/GoalCard.js
+++ src/components/Main/GoalCard/GoalCard.js
@@ -1,45 +1,47 @@
 import React from 'react';
-
+import imageURL from './icon/foto3.png';
 import classes from './GoalCard.module.scss'
 import five from './icon/high-five.png'
 import comment from './icon/chat.png'
 
-export function GoalCard({id, name, title, description, imageURL, time, progress}) {
+export function GoalCard({id, name, title, description, time, progress}) {
     return (
-        <div className={classes.main}> 
-            <div className={classes.main__profile}>
-                <img src={imageURL} alt='photoprofile'/>
-            </div>
-            <div className={classes.main__name}>
-                {name}
-            </div>
-            <div className={classes.main__goal}>
-            </div>
-            <div className={classes.main__goaltext}>
-                {title}
-            </div>
-            <div className={classes.main__time}>
-                {time}
+        <div className={classes.main}>
+            <div className={classes.main__top}>
+                <div className={classes.main__profile}>
+                    <img src={imageURL} alt='photoprofile'/>
+                </div>
+                <div className={classes.main__name}>
+                    {name}
+                </div>
+                <div className={classes.main__goal}>
+                    {title}
+                </div>
+                <div className={classes.main__time}>
+                    Cегодня в 19:32
+                </div>
             </div>
             <div className={classes.main__text}>
-                {description}
+                Я еще никогда не был так воодушевлен проектом, мне хочется погрузиться в него с головой. Сегодня я начинаю свой путь стартапера, я в предвкушении чудесных эмоций и огромного количества неудач. Буду держать вас в курсе!
             </div>
-            <div className={classes.main__percent}>
-                {progress}
-            </div>
-            <div className={classes.main__progressbar}>
-            </div>
-            <div className={classes.main__progress}>
-            </div>
-            <div className={classes.main__like}>
-                <img src={five} alt='five' className={classes.main__icon}/>
-            </div>
-            
-            <div className={classes.main__numberlike}>
-                1К
-            </div>
-            <div className={classes.main__comment}>
-                <img src={comment} alt='comment' className={classes.main__commenticon}/>
+            <div className={classes.main__footer}>
+                <div className={classes.main__percent}>
+                    {progress}
+                </div>
+                <div className={classes.main__progressbar}>
+                    <div className={classes.main__progress}>
+                    </div>
+                </div>
+                <div className={classes.main__like}>
+                    <img src={five} alt='five' className={classes.main__icon}/>
+                    <div className={classes.main__numberlike}>
+                    1К
+                    </div>
+                </div>
+                
+                <div className={classes.main__comment}>
+                    <img src={comment} alt='comment' className={classes.main__commenticon}/>
+                </div>
             </div>
         </div>
     )
